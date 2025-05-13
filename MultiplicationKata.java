3741dc3dc29fa85f953fb1eb08a9de115fe32f8a
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-05-13 02:10:30 +0100

    Multiplication table in function

--- MultiplicationKata.java
+++ MultiplicationKata.java
@@ -3,11 +3,18 @@ public class MultiplicationKata {
 	public static int multiplication (int startPoint, int endPoint){
 
 	int multiple = 0;
+	for (int k = startPoint; k <= endPoint; k++){
+	System.out.print("\t" + k);
+	}
+	System.out.println("");
+	for (int l = startPoint; l <= endPoint; l++){
+	System.out.printf("%s", "--------");
+	}
+	System.out.print("---");
 
 	for (int i = startPoint; i <= endPoint; i++){
-	
 	System.out.print(" ".repeat(i));
-	System.out.print("\n" + i + " |");
+	System.out.printf("\n%2d %s", i, "|");
 	for (int j = 1; j <= endPoint; j++){
 	multiple = (i * j);
 	System.out.print("\t" + multiple);
