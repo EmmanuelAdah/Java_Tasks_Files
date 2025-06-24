6fbed56dbb9d254f12b4be9725d0d9040fbad915
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-24 12:52:15 +0100

    Estore task

---
+++ OOP/Estore.java
@@ -0,0 +1,42 @@
+public class Estore {
+    private String name;
+    private int age;
+    private String email;
+    private String homeAddress;
+    private String password;
+    private String phoneNumber;
+    
+
+    public Estore(String name, int age, String email, String homeAddress, String password, String phoneNumber) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.homeAddress = homeAddress;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getHomeAddress(){
+        return this.homeAddress;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+}
