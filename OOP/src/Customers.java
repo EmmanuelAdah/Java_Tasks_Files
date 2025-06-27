cd19f2781a1169d1ae3627114293ca8a8a9bb174
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-27 23:45:13 +0100

    E-Store App

---
+++ OOP/src/Customers.java
@@ -0,0 +1,7 @@
+public class Customers extends Users {
+
+    public Customers(String name, int age, String email, String homeAddress, String password, String phoneNumber) {
+        super(name, age, email, homeAddress, password, phoneNumber);
+    }
+
+}
