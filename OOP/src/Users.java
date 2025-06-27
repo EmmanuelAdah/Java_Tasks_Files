7a047c56a36ae93ae8bb6d6579fc8f31b64a7b3b
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-27 23:28:43 +0100

    Users class

--- OOP/src/Users.java
+++ OOP/src/Users.java
@@ -4,7 +4,7 @@ public class Users {
     private final String email;
     private final String homeAddress;
     private final String password;
-    private final String phoneNumber;
+    private String phoneNumber;
 
     public Users(String name, int age, String email, String homeAddress, String password, String phoneNumber) {
         this.name = name;
@@ -12,7 +12,10 @@ public class Users {
         this.email = email;
         this.homeAddress = homeAddress;
         this.password = password;
-        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPhoneNumber(){
+        phoneNumber = phoneNumber;
     }
 
     public String getName(){
