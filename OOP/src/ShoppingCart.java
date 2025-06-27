7ca9ce40de5057edf7256a12dec64779b45fcfb2
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-27 23:56:29 +0100

    Shopping Cart

--- OOP/src/ShoppingCart.java
+++ OOP/src/ShoppingCart.java
@@ -3,4 +3,5 @@ public class ShoppingCart extends Items {
     public ShoppingCart(int quantity, String productName, String productID, double price, String productDescription) {
         super(quantity, productName, productID, price, productDescription);
     }
+
 }
