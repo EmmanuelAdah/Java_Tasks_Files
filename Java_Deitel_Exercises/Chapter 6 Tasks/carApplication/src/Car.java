a3c65a9472df7b733a9d607825a0d3118408c1ef
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-06-30 21:01:41 +0100

    Car Application..

---
+++ Java_Deitel_Exercises/Chapter 6 Tasks/carApplication/src/Car.java
@@ -0,0 +1,43 @@
+public class Car {
+    private String model;
+    private String year;
+    private double price;
+
+    public void setCarModel(String model) {
+        this.model = model;
+    }
+
+    public String getCarModel() {
+        return model;
+    }
+
+    public void setCarYear(String year) {
+        this.year = year;
+    }
+
+    public String getCarYear() {
+        return year;
+    }
+
+    public void setCarPrice(double price) {
+        if (price >= 0) {
+            this.price = price;
+        }
+    }
+
+    public double getCarPrice() {
+        return price;
+    }
+
+    public void discountAt_5_Percent(double price) {
+        double PERCENTAGE = 100;
+        double discount = (5 / PERCENTAGE) * price;
+        this.price = price - discount;
+    }
+
+    public void discountAt_7_Percent(double price) {
+        double PERCENTAGE = 100;
+        double discount = (7 / PERCENTAGE) * price;
+        this.price = price - discount;
+    }
+}
--- Java_Deitel_Exercises/Chapter 6 Tasks/carApplication/test/CarApplicationTest.java
+++ Java_Deitel_Exercises/Chapter 6 Tasks/carApplication/test/CarApplicationTest.java
@@ -3,11 +3,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CarApplicationTest {
 
-    CarApplication car = new CarApplication();
+    Car car = new Car();
 
     @Test
     public void checkForCarModel(){
         car.setCarModel("TD5621");
         assertEquals("TD5621", car.getCarModel());
+    }
+
+    @Test
+    public void checkForCarYear(){
+        car.setCarYear("2024");
+        assertEquals("2024", car.getCarYear());
+    }
+
+    @Test
+    public void checkForCarPrice(){
+        car.setCarPrice(2_000);
+        assertEquals(2_000.00, car.getCarPrice());
+    }
+
+    @Test
+    public void checkForNegativeCarPrice(){
+        car.setCarPrice(-2_000);
+        assertEquals(0.0, car.getCarPrice());
+    }
+
+    @Test
+    public void checkForPriceDiscountAt_5_Percent(){
+        car.discountAt_5_Percent(2000);
+        assertEquals(1900.00, car.getCarPrice());
+    }
+
+    @Test
+    public void checkForPriceDiscountAt_7_Percent(){
+        car.discountAt_7_Percent(2000);
+        assertEquals(1860.00, car.getCarPrice());
     }
 }
