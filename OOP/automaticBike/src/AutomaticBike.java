b96d0f92ffb2f840cb713b3b8289599e2195eadb
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-07-02 04:16:56 +0100

    Automatic Bike App

--- OOP/automaticBike/src/AutomaticBike.java
+++ OOP/automaticBike/src/AutomaticBike.java
@@ -1,6 +1,8 @@
 public class AutomaticBike {
     private boolean status;
     private String bikeEngine;
+    private int gear;
+    private int speed;
 
     public boolean getStatus() {
         return status;
@@ -9,4 +11,22 @@ public class AutomaticBike {
     public void setStatus(String bikeEngine) {
         if (bikeEngine.equalsIgnoreCase("on")) status = true;
     }
+
+    public int getGear() {
+        return gear;
+    }
+
+    public void setGear(int speed) {
+        if (speed > 0 && speed <= 20){
+            gear = 1;
+        } else if (speed > 20 && speed <= 30){
+            gear = 2;
+        }  else if (speed > 30 &&  speed <= 40){
+            gear = 3;
+        } else if (speed > 40){
+            gear = 4;
+        }
+    }
+
+
 }
--- OOP/automaticBike/test/AutomaticBikeTest.java
+++ OOP/automaticBike/test/AutomaticBikeTest.java
@@ -1,25 +1,39 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AutomaticBikeTest {
 
     AutomaticBike bike = new AutomaticBike();
 
     @Test
-    public void bikeCnaTurnOffTest(){
+    public void bikeIsOffTest(){
         assertFalse(bike.getStatus());
     }
 
     @Test
-    public void bikeCnaTurnOnTest(){
+    public void bikeTurnOnTest(){
         bike.setStatus("ON");
         assertTrue(bike.getStatus());
     }
 
     @Test
-    public void bikeCnaTurnTest(){
-        bike.setStatus("ON");
+    public void bikeGearIsZeroTest(){
+        assertEquals(0,bike.getGear());
+    }
+
+    @Test
+    public void bikeGearIncreaseTest(){
+        bike.setGear(20);
+        assertEquals(1,bike.getGear());
+
+        bike.setGear(21);
+        assertEquals(2,bike.getGear());
+
+        bike.setGear(32);
+        assertEquals(3,bike.getGear());
+
+        bike.setGear(41);
+        assertEquals(4,bike.getGear());
     }
 }
