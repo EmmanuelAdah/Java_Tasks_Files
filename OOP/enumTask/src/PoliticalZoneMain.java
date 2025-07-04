f4e5b47c55dccdad2dc10c2ba430fbe0d82d6138
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-07-04 20:59:18 +0100

    Geo-political Zone Inter-preter

---
+++ OOP/enumTask/src/PoliticalZoneMain.java
@@ -0,0 +1,17 @@
+import geoPoliticalZone.GeoPoliticalZone;
+
+import java.util.Scanner;
+
+public class PoliticalZoneMain {
+    public static void main(String... args) {
+        GeoPoliticalZone zone = new GeoPoliticalZone();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter state: ");
+        String state = sc.nextLine();
+
+        zone.setPoliticalZone(state);
+
+        System.out.printf("%s is in %s geo-political zone", zone.getState(), zone.getPoliticalZone());
+    }
+}
--- OOP/enumTask/src/geoPoliticalZone/GeoPoliticalZone.java
+++ OOP/enumTask/src/geoPoliticalZone/GeoPoliticalZone.java
@@ -1,10 +1,8 @@
 package geoPoliticalZone;
-import java.util.Scanner;
 
 public class GeoPoliticalZone {
     private String politicalZone;
     private String state;
-    private String checkState;
 
     public  void setPoliticalZone(String user) {
         PoliticalZone[] politicalZone = PoliticalZone.values();
@@ -24,17 +22,5 @@ public class GeoPoliticalZone {
 
     public String getState() {
         return state;
-    }
-
-
-    public void main(String... args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter state: ");
-        String state = sc.nextLine();
-
-        setPoliticalZone(state);
-
-        System.out.printf("%s is in %s geo-political zone", getState(), getPoliticalZone());
     }
 }
