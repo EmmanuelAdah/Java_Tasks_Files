6fdb27d151385d9f46f533af60d86e20fc727e67
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-07-13 19:06:05 +0100

    Movie Rating System

--- OOP/movieRatingSystem/src/movie_Ratings/movieRatings/MovieRatingMain.java
+++ OOP/movieRatingSystem/src/movie_Ratings/movieRatings/MovieRatingMain.java
@@ -7,12 +7,16 @@ public class MovieRatingMain {
         Rating rating = new Rating();
 
         String movieMenu = """
+                ===========================
                 Welcome to Vince Movies
+                ===========================
                 Press:-
+                ---------------------------
                 1 -> Add movie
                 2 -> Rate a movie
                 3 -> View average ratings
                 4 -> Exit
+                ===========================
                 """;
         boolean movies = true;
         while(movies){
@@ -20,21 +24,25 @@ public class MovieRatingMain {
             int choice = sc.nextInt();
             switch(choice){
                 case 1:
-                    System.out.println("Enter movie title: ");
+                    System.out.print("Enter movie title: ");
                     String title = sc.next();
                     rating.addMovieToList(title);
                     rating.addListToRatingList();
                     break;
                 case 2:
-                    System.out.println("Enter movie name: ");
+                    System.out.print("Enter movie name: ");
                     String name = sc.next();
-                    System.out.println("Enter movie rating: ");
+                    System.out.print("Enter movie rating: ");
                     float rate = sc.nextFloat();
                     break;
                 case 3:
-                    rating.averageMovieRating();
-                    break;
+                    if (!rating.movieListIsEmpty()){
+                        rating.averageMovieRating();
+                    } else {
+                        System.out.println("There is no movie in the list");
+                    } break;
                 case 4:
+                    System.out.println("Thank you for using Vince Movies \nGoodBye!...");
                     movies = false;
             }
         }
