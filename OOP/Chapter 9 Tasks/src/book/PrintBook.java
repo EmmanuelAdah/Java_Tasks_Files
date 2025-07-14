5a3b03f65a48002a7a456fa0b247bae2765c6e70
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-07-14 20:34:04 +0100

    Book App

---
+++ OOP/Chapter 9 Tasks/src/book/PrintBook.java
@@ -0,0 +1,22 @@
+package book;
+
+public class PrintBook extends Book {
+    private String publisher;
+    private String isbn;
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+}
--- OOP/Chapter 9 Tasks/test/book/BookTest.java
+++ OOP/Chapter 9 Tasks/test/book/BookTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BookTest {
-    Book book = new Book();
+    PrintBook book = new PrintBook();
 
     @Test
     public void bookTitleTest(){
@@ -16,5 +16,17 @@ public class BookTest {
     public void bookAuthorTest(){
         book.setBookAuthor("Mr. Dapo");
         assertEquals("Mr. Dapo", book.getBookAuthor());
+    }
+
+    @Test
+    public void bookIsbnTest(){
+        book.setIsbn("27327TR623");
+        assertEquals("27327TR623", book.getIsbn());
+    }
+
+    @Test
+    public void bookPublisherTest(){
+        book.setPublisher("WatchTower's Lmt");
+        assertEquals("WatchTower's Lmt", book.getPublisher());
     }
 }
