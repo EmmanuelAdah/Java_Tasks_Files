b0c7e76486b44195846a1cedf05c7edbcbfa1cdf
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-07-16 22:12:49 +0100

    Diary Test

---
+++ OOP/diaries/test/diaries/DiaryTest.java
@@ -0,0 +1,21 @@
+package diaries;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DiaryTest {
+    Diary diary = new Diary("Adah02", "trey5");
+
+    @Test
+    public void diaryIsLockedTest(){
+        diary.lockDiary();
+        assertTrue(diary.isLocked());
+    }
+
+    public void diaryIsUnlockedTest(){
+        diary.unlockDiary("trey5");
+        assertFalse(diary.isLocked());
+    }
+}
