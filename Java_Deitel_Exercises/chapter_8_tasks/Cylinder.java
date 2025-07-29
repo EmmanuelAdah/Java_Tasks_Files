f4e4c12dc1b81cdbcd25b2e2f26bbcf45dd95544
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-07-29 23:33:37 +0100

    Calculating the volume of gas cylinder

---
+++ Java_Deitel_Exercises/chapter_8_tasks/Cylinder.java
@@ -0,0 +1,37 @@
+package chapter_8_tasks;
+
+public class Cylinder {
+    private double radius;
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        if (radius <= 0) throw new IllegalArgumentException("Radius must be greater than zero.");
+        this.radius = radius;
+        if (height <= 0) throw new IllegalArgumentException("Height must be greater than zero.");
+        this.height = height;
+    }
+
+    public void setRadius(double radius) {
+        if (radius <= 0) throw new IllegalArgumentException("Radius must be greater than zero.");
+        this.radius = radius;
+    }
+
+    public void setHeight(double height) {
+        if (height <= 0) throw new IllegalArgumentException("Height must be greater than zero.");
+        this.height = height;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double volumeOfCylinder(double radius, double height){
+        return Math.PI * radius * height;
+    }
+
+
+}
---
+++ Java_Deitel_Exercises/chapter_8_tasks/CylinderTest.java
@@ -0,0 +1,14 @@
+package chapter_8_tasks;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CylinderTest {
+    Cylinder cylinder = new Cylinder(5.0, 10.0);
+
+    @Test
+    public void cylinderTest() {
+        assertEquals(62.84, cylinder.volumeOfCylinder(2.0, 10.0));
+    }
+}
