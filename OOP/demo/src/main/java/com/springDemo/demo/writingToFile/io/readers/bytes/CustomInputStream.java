4e07e676a5b0bd3b25651b098b409f4f4224075d
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-10-15 20:39:48 +0100

    for reading files via java input stream

---
+++ OOP/demo/src/main/java/com/springDemo/demo/writingToFile/io/readers/bytes/CustomInputStream.java
@@ -0,0 +1,19 @@
+package com.springDemo.demo.writingToFile.io.readers.bytes;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class CustomInputStream {
+    public static void main(String[] args) {
+        String filename = "C:\\Users\\DELL\\Account.java";
+//
+//        try (DataInputStream dataInputStream = new DataInputStream(filename)){
+//
+//
+//            byte[] number = dataInputStream.readAllBytes();
+//            System.out.println("You entered: " + number);
+//        } catch (IOException ex) {
+//            ex.printStackTrace();
+//        }
+    }
+}
